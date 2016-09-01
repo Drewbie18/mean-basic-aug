2a800fc086ea3211fd5caeb6190abcbddb1dd75b
Drewbie18 <drew.payette@gmail.com>
2016-08-31 22:11:32 -0400

    updated controller setup

--- client/public/js/angular-app.js
+++ client/public/js/angular-app.js
@@ -27,43 +27,7 @@ angularApp.config(function ($routeProvider) {
 });
 
 
-angularApp.controller('landing-controller', function ($scope, $http) {
-    console.log('landing-controller is here');
 
 
-    $scope.sayHi = function () {
-        $scope.hello = "Hello MotherFUCKER!";
-    }
 
 
-    $scope.getResponse = function () {
-
-        $http({
-            method: 'GET',
-            url: '/api/item/socks/13'
-        }).then(function successCallback(response) {
-
-            console.log(response);
-
-            $scope.responseId = response.data._id;
-            $scope.responseName = response.data.name;
-            $scope.responseText = response.data.text;
-        }, function errorCallback(response) {
-
-            console.log('There was an error');
-
-        });
-
-    }
-});
-
-
-angularApp.controller('wahlin-1-controller', function ($scope, $http) {
-    console.log('wahlin-1-controller is here');
-
-});
-
-angularApp.controller('wahlin-2-controller', function ($scope, $http) {
-    console.log('wahlin-2-controller is here');
-
-});
---
+++ client/public/js/controllers/wahlinController-1.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Drew on 2016-08-31.
+ */
+angularApp.controller('wahlin-1-controller', function ($scope, $http) {
+    console.log('wahlin-1-controller is here');
+
+});
