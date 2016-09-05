4418abf18b172c49a6944f6bdd37728338ed39d3
Drewbie18 <drew.payette@gmail.com>
2016-09-05 17:13:52 -0400

    updated

--- client/public/js/controllers/wahlinController-2.js
+++ client/public/js/controllers/wahlinController-2.js
@@ -5,15 +5,21 @@
 
         $scope.title = 'THIS IS THE SECOND PAGE';
 
+        $scope.getTexts = function () {
+            getTexts.getTexts().then(function successCallback(response) {
 
-        
+                console.log(response.data);
 
+            }, function errorCallback(response) {
 
+                console.log('There was an error: ' + response);
 
+            });
+        }
 
 
     }
-     //use the inject service to ensure that is there is minification the injected
+    //use the inject service to ensure that is there is minification the injected
     //services are not overwritten to s and h as minifiers tend to do.
     //all factories, services, values must be added here to function
     wahlinTwoConroller.$inject = ['$scope', 'getTexts'];
