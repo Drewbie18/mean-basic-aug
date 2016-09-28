113120e44af4e0f3cad9fcb573e6da63a5eaefaf
Drew Payette <apayette@ca.ibm.com>
2016-09-28 12:26:19 -0400

    updated naming and added to index

--- client/public/js/directives/d3Directive.js
+++ client/public/js/directives/d3Directive.js
@@ -3,10 +3,11 @@
  */
 (function () {
 
-    var d3Directive = function () {
+    var dThree = function () {
 
         //define the template the the directive will render
-        var template = '<svg width="850" height="200"></svg>'
+        var template = '<svg width="850" height="200"></svg><br>'+
+            '<h3>{{variable}}</h3>';
 
         //define the controller that will be passed into the return.
         controller = ['$scope', function ($scope) {
@@ -24,6 +25,6 @@
     };
 
     //register the directive with the angular app
-    angular.module('angular-app').directive('d3', d3Directive);
+    angular.module('angular-app').directive('dThree', dThree);
 
 }());
