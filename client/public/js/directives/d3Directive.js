d0993b323fbf195062977663ab5b495933394cb7
Drew Payette <apayette@ca.ibm.com>
2016-09-29 15:18:27 -0400

    added working d3

--- client/public/js/directives/d3Directive.js
+++ client/public/js/directives/d3Directive.js
@@ -6,12 +6,22 @@
     var dThree = function () {
 
         //define the template the the directive will render
-        var template = '<svg width="850" height="200"></svg><br>'+
-            '<h3>{{variable}}</h3>';
+        var template = '<svg width="850" height="200"></svg><br>' +
+            '<h3>{{variable}}</h3>' +
+            '<h3>{{minVal}}</h3>';
 
         //define the controller that will be passed into the return.
-        controller = ['$scope','d3Service', function ($scope, d3Service) {
+        controller = ['$scope', 'd3Service', function ($scope, d3Service) {
             $scope.variable = 'Hello World';
+
+            var data = [10, 15, 1, 2, 5, 6, 11];
+
+            console.log(d3Service);
+           
+
+            console.log(d3Service.min(data));
+            $scope.minVal = d3Service.min(data);
+
 
         }];
 
